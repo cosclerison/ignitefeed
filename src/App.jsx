0e18c9980c90fb9418d8bcec5c8a9d998e26d6fb
clerison <cosclerison@gmail.com>
2022-08-26 20:49:41 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-function App() {
+import { Post } from './Post';
+
+export function App() {
   return (
+    <div>
     <h1>Ignite Feed</h1>
+    <Post 
+      author="Clerison" 
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
+    />
+    <Post 
+      author="Juliana" 
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
+    />
+    <Post 
+      author="Bianca" 
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
+    />
+    </div>
   )
 }
-
-export default App
