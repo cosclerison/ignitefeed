218e421838473c8a5d33ef11ae9a06735af2b10a
clerison <cosclerison@gmail.com>
2022-08-26 21:13:08 -0300

    Component Style Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+import "./style.css";
 
 export function App() {
   return (
     <div>
-    <h1>Ignite Feed</h1>
+      <Header />
     <Post 
       author="Clerison" 
       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
