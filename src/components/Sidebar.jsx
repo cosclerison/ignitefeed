15484777fd6adc43221f563aa7e092402ba1cd84
clerison <cosclerison@gmail.com>
2022-08-29 18:34:42 -0300

    Sidebar Avatar icons botton

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,12 +11,18 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src="https://github.com/cosclerison.png"
+                />
+
                 <strong>Clerison Oliveira</strong>
                 <span>Developer Jr</span>
             </div>
 
             <footer>
                  <a href='#'>
+                    <PencilLine size={20} />
                     Editar seu perfil
                  </a>
             </footer>
