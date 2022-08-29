0135f3b3b9667eb7b1bfe567017e25043f54fe89
clerison <cosclerison@gmail.com>
2022-08-29 17:37:26 -0300

    ~Sidebar~

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-    <Post 
-      author="Clerison" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
-    />
-    <Post 
-      author="Juliana" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
-    />
-    <Post 
-      author="Bianca" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
-    />
+
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+        <main>
+          <Post 
+            author="Clerison" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
+            />
+          <Post 
+            author="Juliana" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
+            />
+          <Post 
+            author="Bianca" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto, cumque est blanditiis animi, expedita eos illo pariatur, esse dolore quibusdam. Architecto, cumque incidunt! Eligendi sit cupiditate enim saepe quos." 
+            />
+        </main>
+    
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1635863138275-d9b33299680b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+                alt="Iron" 
+            />
+
+            <div className={styles.profile}>
+                <strong>Clerison Oliveira</strong>
+                <span>Developer Jr</span>
+            </div>
+
+            <footer>
+                 <a href='#'>
+                    Editar seu perfil
+                 </a>
+            </footer>
+        </aside>
+    )
+}
