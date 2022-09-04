f82a53d1c09dccc38811af89b664706906383c69
Clerison <cosclerison@gmail.com>
2022-09-03 21:22:38 -0300

    Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,6 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
 export function Post() {
@@ -6,7 +8,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar}
+                    <Avatar
                       src="https://github.com/cosclerison.png"
                       alt="Avatar" 
                     />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -11,10 +13,8 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img
-                    className={styles.avatar}
-                    src="https://github.com/cosclerison.png"
-                />
+                <Avatar 
+                    src="https://github.com/cosclerison.png" />
 
                 <strong>Clerison Oliveira</strong>
                 <span>Developer Jr</span>
